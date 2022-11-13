b598a2fd9c600d8faaf3024fb876b857c4b47126
Jakub Grabowski <112199@student.uthrad.pl>
2022-11-13 09:14:22 +0100

    Dodaj pliki projektów.

---
+++ HoneyVaultApp/HoneyVault.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoneyVaultApp
+{
+    static class HoneyVault
+    {
+        private static string StatusReport
+        {
+            get
+            {
+                string warnings = "";
+                if (honey < LOW_LEVEL_WARNING) warnings += "\nMAŁO MIODU - DODAJ PRODUCENTÓW MIODU!";
+                if (nectar < LOW_LEVEL_WARNING) warnings += "\nMALO NEKTARU - DODAJ PRODUCENTÓW NEKTARU!";
+                return $"\nILOŚĆ MIODU: {honey} ILOŚĆ NEKTARU: {nectar}\n" + warnings;
+            }
+        }
+        const float NECTAR_CONVERSION_RATIO = 0.19F;
+        const float LOW_LEVEL_WARNING = 10f;
+        private static float honey = 25f;
+        private static float nectar = 100f;
+        /// <summary>
+        /// Zamienia nektar na miód, jesli amount jest wieksze niż ilość nekatru, zamienia cały pozostały nektar
+        /// </summary>
+        /// <param name="amount">wartość zamiany nektaru w miód</param>
+        public static void ConvertNectarToHoney(float amount)
+        {
+
+            if (amount > nectar)
+            {
+                honey += (nectar * NECTAR_CONVERSION_RATIO);
+                nectar = 0;
+            }
+            else
+            {
+                honey += (amount * NECTAR_CONVERSION_RATIO);
+                nectar -= amount;
+            }
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (amount <= honey)
+            {
+                honey -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0) nectar += amount;
+        }
+
+
+    }
+}
